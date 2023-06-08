5fa714f46a964d7c8122bd790f7a403f87f697a0
Hyeseong-Myeong <a01025819869@gmail.com>
2023-06-08 13:37:53 +0900

    update

---
+++ HW/HW2.py
@@ -0,0 +1,18 @@
+str = "My name is Hyeseong Myeong"
+
+tentimes = ""
+for i in range (0, 10, 1) :
+    tentimes += str
+
+
+print(len(str))
+print(tentimes)
+print(str[0])
+print(str[:4])
+print(str[-4:])
+print(str[::-1])
+print(str[1:-1])
+print(str.upper())
+print(str.lower())
+print(str.replace('a', 'e'))
+
